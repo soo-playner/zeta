f832bf89b0ae00edb823e1078026f2045a634f04
soo-willsoft <soo@logcompany.co.kr>
2022-01-14 18:40:50 +0900

    ADD :: 회원승급시 마이닝 데이터 기록생성 + DB처리 + JSON DATA

--- util/binary_tree_add.php
+++ util/binary_tree_add.php
@@ -59,57 +59,6 @@ function get_brecommend_down($mb_id, $m_id, $ca_id)
 	}
 }
 
-function calc_sale($mb_id, $to_date){
-
-	$sql= " select sales_day g5_member where sales_day<>'".date('Y-m-d')."'";
-	$salesday=sql_fetch($sql);
-	if(($salesday['sales_day']) || ($update=='y') ){
-		$sql= " UPDATE g5_member  SET mb_my_sales=0, noo_my_sales=0,  mon_my_sales=0 ,day_my_sales=0, mb_habu_sum=0, noo_habu_sum=0, mon_habu_sum=0, habu_day_sales=0, sales_day=''";
-		sql_query($sql);
-		//echo $sql."<br>sales_day가 오늘날짜와 다르거나 강제업데이트 0으로 update...<br><br>";
-	}
-	$sql = "SELECT mb_id, SUM(pv) AS pv FROM g5_shop_order WHERE DATE_FORMAT(od_receipt_time,'%Y-%m-%d')='$to_date' and mb_id='".$mb_id."'";
-	$result = sql_query($sql);
-	//echo $sql;
-	if($to_date==''){
-		$to_date=date('Y-m-d');
-	}
-	for ($i=0; $row=sql_fetch_array($result); $i++) {   
-		$comp=$row['mb_id'];
-		$today_sales=$row['pv'];	
-		$history_cnt=0;
-		while(  ($comp!='admin')  || ($comp!='coolrunning')  ){   
-			$sql = " SELECT mb_id,mb_name,  mb_brecommend FROM g5_member WHERE mb_id= '".$comp."'";
-			$recommend = sql_fetch($sql);
-			$mb_id=$recommend['mb_id'];
-			$mb_name=$recommend['mb_name'];
-			$sql3='';
-			//echo $comp;
-			//위로 연속 올라가는 부분 추천인으로 올라갈지 바이너리추천인으로 올라갈지 결정
-			$recom=$recommend['mb_brecommend'];
-			if(($mb_name=='본사')  || ($mb_id=='')  ) { 
-				//echo "admin , 본사 혹은 ''을 만나 정지됨"; 
-				//print json_encode(array("message"=>"admin , 본사 혹은 ''을 만나 정지됨"));
-				break;
-			}
-			$sql3 = " update g5_member set sales_day='".$to_date."',";
-			if($history_cnt==0)
-			{ 
-				$sql3 .= " mb_my_sales=mb_my_sales	+ ".$today_sales;
-			}else{
-				$sql3 .= " habu_day_sales=habu_day_sales+".$today_sales;
-			}
-			$sql3 .= " where mb_id='".$comp."'";
-			sql_query($sql3);
-			//echo $history_cnt.'--'.$sql3.'<br>';
-			$comp=$recom;
-			$history_cnt++;	
-		} // while
-		$history_cnt=0;
-		$today_sales=0;
-	} //for
-}
-
 $now_date = date("Y-m-d H:i:s",time()); 
 
 // binary_parent - 바이너리 트리에서 직속 부모 
@@ -133,7 +82,7 @@ sql_query("update g5_shop_order set od_receipt_time='".$now_date."' where mb_id 
 
 sql_query("update g5_member set mb_brecommend='".$binary_parent."',mb_lr={$mb_lr}, mb_brecommend_type='".$set_type."',mb_bre_time = '".$now_date."' where mb_id='".$selected."'");
 
-calc_sale($selected, date('Y-m-d'));
+// calc_sale($selected, date('Y-m-d'));
 
 $ret = sql_fetch("select * from g5_member_bclass where mb_id='{$member['mb_id']}' and c_id = '{$binary_parent}'"); 
 if($ret){
--- util/brecommend.php
+++ util/brecommend.php
@@ -2,7 +2,7 @@
 // 후원 바이너리 관련 
 $brcomm_arr = [];
 
-function brecom_list($mb_id){
+/* function brecom_list($mb_id){
     global $config,$brcomm_arr,$debug;
 	$origin = $mb_id;
 
@@ -16,8 +16,6 @@ function brecom_list($mb_id){
     $brcomm_arr = [];
     array_push($brcomm_arr, $leg_list[0]);
     $manager_list_L = brecommend_array($L_member, 0);
-    /* echo "<br><br> L ::<br>";
-    print_R($manager_list_L); */
 
     $brcomm_arr = [];
     array_push($brcomm_arr, $leg_list[1]);
@@ -25,46 +23,83 @@ function brecom_list($mb_id){
         
     return array($manager_list_L,$manager_list_R);
         
-}
+} */
 
+function return_brecommend($mb_id,$limit,$binding = false){
+    global $config, $brcomm_arr, $debug;
+    $origin = $mb_id;
+
+    list($leg_list, $cnt) = brecommend_direct($mb_id);
+
+    $L_member = $leg_list[0]['mb_id'];
+    $R_member = $leg_list[1]['mb_id'];
+    
+    
+    $brcomm_arr_L = [];
+    array_push($brcomm_arr_L, $leg_list[0]);
+    $manager_list_L = brecommend_array($L_member, 1 , $limit);
+    $brcomm_arr_L = array_merge($brcomm_arr_L,arr_sort($manager_list_L,'count'));
+
+    $brcomm_arr = [];
+    $brcomm_arr_R = [];
+    array_push($brcomm_arr_R, $leg_list[1]);
+    $manager_list_R = brecommend_array($R_member, 1 , $limit);
+    $brcomm_arr_R = array_merge($brcomm_arr_R,arr_sort($manager_list_R,'count'));
+   
+    if(!$binding){
+        return array($brcomm_arr_L,$brcomm_arr_R); 
+    }else{
+        return array_merge($brcomm_arr_L,$brcomm_arr_R);
+    }
+    
+}
 
 
 // 후원인 하부 회원 
-function brecommend_array($brecom_id, $count)
+function brecommend_array($brecom_id, $count, $limit =0)
 {
-	global $brcomm_arr;
+    global $brcomm_arr;
 
-	// $new_arr = array();
-	$b_recom_sql = "SELECT mb_id,grade,mb_rate,mb_brecommend_type from g5_member WHERE mb_brecommend='{$brecom_id}' ";
-	$b_recom_result = sql_query($b_recom_sql);
-	$cnt = sql_num_rows($b_recom_result);
+    // $new_arr = array();
+    $b_recom_sql = "SELECT mb_id,grade,mb_rate,mb_save_point,mb_brecommend_type, {$count} as count from g5_member WHERE mb_brecommend='{$brecom_id}' ORDER BY mb_brecommend_type ASC ";
+    $b_recom_result = sql_query($b_recom_sql);
+    $cnt = sql_num_rows($b_recom_result);
+    
+    if($limit != 0 && $count >= $limit){
+        
+    }else{
+        if ($cnt < 1) {
+            // 마지막
+        } else {
+            ++$count;
 
-	if ($cnt < 1) {
-		// 마지막
-	} else {
-		++$count;
-		while ($row = sql_fetch_array($b_recom_result)) {
-			brecommend_array($row['mb_id'], $count);
-            // print_R($count.' :: '.$row['mb_id'].' | type ::'.$row['grade']);
-            // $brcomm_arr[$count]['id'] = $brecom_id;
-            array_push($brcomm_arr, $row);
-		}
-	}
-	return $brcomm_arr;
+            while ($row = sql_fetch_array($b_recom_result)) {
+                brecommend_array($row['mb_id'], $count, $limit);
+                // print_R($count.' :: '.$row['mb_id'].' | type ::'.$row['grade']);
+                // $brcomm_arr[$count]['count'] = $count;
+                array_push($brcomm_arr, $row);
+            }
+            
+        }
+    }
+    
+    return $brcomm_arr;
 }
 
 
-function brecommend_direct($mb_id){
-   
+
+function brecommend_direct($mb_id)
+{
+
     $down_leg = array();
-    $sql = "SELECT mb_id,grade,mb_rate,mb_save_point,mb_brecommend_type FROM g5_member where mb_brecommend = '{$mb_id}' AND mb_brecommend != '' ORDER BY mb_brecommend_type ASC ";
+    $sql = "SELECT mb_id,grade,mb_rate,mb_save_point,mb_brecommend_type, 1 AS count FROM g5_member where mb_brecommend = '{$mb_id}' AND mb_brecommend != '' ORDER BY mb_brecommend_type ASC ";
     $sql_result = sql_query($sql);
     $cnt = sql_num_rows($sql_result);
 
-    while($result = sql_fetch_array($sql_result) ){
-        array_push($down_leg,$result);
-    } 
-    return array($down_leg,$cnt);
+    while ($result = sql_fetch_array($sql_result)) {
+        array_push($down_leg, $result);
+    }
+    return array($down_leg, $cnt);
 }
 
 ?>
